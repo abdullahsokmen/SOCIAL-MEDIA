69e72fffc99a142e4d6ce856a57e23a69689a1b6
abdullahsokmen <Abdu11ahsk@icloud.com>
2023-03-31 10:19:17 +0300

    update

--- auth-service/src/main/java/com/abdullah/config/redis/RedisConfiguration.java
+++ auth-service/src/main/java/com/abdullah/config/redis/RedisConfiguration.java
@@ -1,5 +1,6 @@
 package com.abdullah.config.redis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +12,13 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @EnableCaching
 @EnableRedisRepositories
 public class RedisConfiguration {
-
-
+    @Value("${redis_host}")
+    private String host;
+    @Value("${redis_port}")
+    private int port;
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("localhost",6379));
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host,port));
     }
 }
